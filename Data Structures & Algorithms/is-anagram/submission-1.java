61c9529dd4892ab0f56de355bb6351bf16698a21
George Prado <gmeloprado@gmail.com>
2026-04-26 18:39:31 -0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Time Complexity: O(n);
+    // Space Complexity: O(n);
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : t.toCharArray()) {
+            if (!freq.containsKey(ch)) {
+                return false;
+            }
+
+            freq.put(ch, freq.get(ch) - 1);
+            if (freq.get(ch) < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
