788ffaf100c25a2ebb7c3b8a5e4d7cf60e5939a5
George Prado <gmeloprado@gmail.com>
2026-04-27 23:30:19 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Time Complexity: O(n * k log k)
+    // Space Complexity: O(n * k)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
