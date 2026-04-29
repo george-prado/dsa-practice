b30f47b02b0213f93a2b00aac10c52d01e16d01b
George Prado <gmeloprado@gmail.com>
2026-04-29 16:38:26 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            char left = s.charAt(l), right = s.charAt(r);
+
+            if (!Character.isLetterOrDigit(left)) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(right)) {
+                r--;
+                continue;
+            }
+
+            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
