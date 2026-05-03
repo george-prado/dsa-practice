dd15a49d2ccfb5f70d76f5fa97b5d67ad7fba96a
George Prado <gmeloprado@gmail.com>
2026-05-03 15:30:05 -0300

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    // Time Complexity: O(n * m)
+    // Space Complexity: O(n + m)
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        } 
+        
+        if (root == null) {
+            return false;
+        }
+
+        if (sameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean sameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+
+        if (root != null && subRoot != null && root.val == subRoot.val) {
+            return sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right);
+        }
+
+        return false;
+    }
+}
